a809d8d876d049f178291f547769e20aa4e8aa22
段弘 <duanhong169@gmail.com>
2018-05-29 16:24:12 +0800

    Add custom effect sample code

--- app/src/main/java/top/defaults/textbuttonapp/MainActivity.java
+++ app/src/main/java/top/defaults/textbuttonapp/MainActivity.java
@@ -3,20 +3,18 @@ package top.defaults.textbuttonapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 import me.ele.uetool.UETool;
-import top.defaults.view.R;
 import top.defaults.view.TextButton;
+import top.defaults.view.TextButtonEffect;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.button0)
-    TextButton button0;
+    @BindView(R.id.button0) TextButton button0;
     @BindView(R.id.button5) TextButton button5;
 
     @OnClick({R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4})
@@ -44,5 +42,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        button5.setEffect(new TextButtonEffect() {
+            private TextButton textButton;
+
+            @Override
+            public void init(TextButton textButton) {
+                this.textButton = textButton;
+            }
+
+            @Override
+            public void actionDown() {
+                textButton.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void actionUp() {
+                textButton.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
