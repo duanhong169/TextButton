548f6663b43bcb7dfaaa2a6a28bb5d2096e638ad
段弘 <duanhong169@gmail.com>
2018-05-29 17:22:20 +0800

    Enable eleme UETool

--- app/src/main/java/top/defaults/textbuttonapp/MainActivity.java
+++ app/src/main/java/top/defaults/textbuttonapp/MainActivity.java
@@ -3,12 +3,14 @@ package top.defaults.textbuttonapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 import me.ele.uetool.UETool;
+import top.defaults.logger.Logger;
 import top.defaults.view.TextButton;
 import top.defaults.view.TextButtonEffect;
 
@@ -17,9 +19,9 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.button0) TextButton button0;
     @BindView(R.id.button5) TextButton button5;
 
-    @OnClick({R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4})
-    void onClick(View view) {
-
+    @OnClick({R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5})
+    void onClick(TextView view) {
+        Logger.d("Clicked: %s", view.getText());
     }
 
     @OnCheckedChanged(R.id.ue_tool)
